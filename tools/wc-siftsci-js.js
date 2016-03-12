b7acc06a0c074358ac267c8f3cb28033af8ea9aa
Nabeel Sulieman <nabsul@outlook.com>
2016-03-12 15:44:21 -0800

    Fix indentation in JS file

--- tools/wc-siftsci-js.js
+++ tools/wc-siftsci-js.js
@@ -5,31 +5,23 @@
  */
 
 var _sift = _sift || [];
-_sift.push(['_setAccount', _wc_siftsci_js_input_data.js_key]);
-_sift.push(['_setUserId', _wc_siftsci_js_input_data.user_id]);
-_sift.push(['_setSessionId', _wc_siftsci_js_input_data.session_id]);
-_sift.push(['_trackPageview']);
+_sift.push( [ '_setAccount', _wc_siftsci_js_input_data.js_key ] );
+_sift.push( [ '_setUserId', _wc_siftsci_js_input_data.user_id ] );
+_sift.push( [ '_setSessionId', _wc_siftsci_js_input_data.session_id ] );
+_sift.push( [ '_trackPageview' ] );
 
-(
-    function() 
-    {
-        function ls() 
-        {
-            var e = document.createElement('script');
-            e.type = 'text/javascript';
-            e.async = true;
-            e.src = ('https:' == document.location.protocol ? 'https://' : 'http://') 
-                    + 'cdn.siftscience.com/s.js';
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(e, s);
-        }
-        if (window.attachEvent) 
-        {
-            window.attachEvent('onload', ls);
-        } 
-        else 
-        {
-            window.addEventListener('load', ls, false);
-        }
-    }
-)();
+(function() {
+	function ls() {
+		var e = document.createElement('script');
+		e.type = 'text/javascript';
+		e.async = true;
+		e.src = ('https:' === document.location.protocol ? 'https://' : 'http://') + 'cdn.siftscience.com/s.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(e, s);
+	}
+	if (window.attachEvent) {
+		window.attachEvent('onload', ls);
+	} else {
+		window.addEventListener('load', ls, false);
+	}
+}());
