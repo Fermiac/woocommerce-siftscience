0afd8e296039efe93c46a69d34cc28b0ede29f43
Nabeel Sulieman <nabsul@outlook.com>
2016-03-28 08:23:57 -0700

    Update JS file according to latest SiftScience Documentation

--- tools/wc-siftsci-js.js
+++ tools/wc-siftsci-js.js
@@ -1,35 +1,19 @@
 /*
  * Author: Nabeel Sulieman
- * Description: This class handles initializing the SiftSience javascript.
- * License: GPL2
+ * Description: SiftScience client script based on
+ * https://siftscience.com/developers/docs/javascript/javascript-api
  */
 
-var _sift = _sift || [];
-_sift.push(['_setAccount', _wc_siftsci_js_input_data.js_key]);
-_sift.push(['_setUserId', _wc_siftsci_js_input_data.user_id]);
-_sift.push(['_setSessionId', _wc_siftsci_js_input_data.session_id]);
-_sift.push(['_trackPageview']);
+var _sift = window._sift = window._sift || [];
+_sift.push( [ '_setAccount', _wc_siftsci_js_input_data.js_key  ]);
+_sift.push( [ '_setUserId', _wc_siftsci_js_input_data.user_id ] );
+_sift.push( [ '_setSessionId', _wc_siftsci_js_input_data.session_id ] );
+_sift.push( [ '_trackPageview' ] );
 
-(
-    function() 
-    {
-        function ls() 
-        {
-            var e = document.createElement('script');
-            e.type = 'text/javascript';
-            e.async = true;
-            e.src = ('https:' == document.location.protocol ? 'https://' : 'http://') 
-                    + 'cdn.siftscience.com/s.js';
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(e, s);
-        }
-        if (window.attachEvent) 
-        {
-            window.attachEvent('onload', ls);
-        } 
-        else 
-        {
-            window.addEventListener('load', ls, false);
-        }
-    }
-)();
+(function( d, s, id ) {
+	var e, f = d.getElementsByTagName( s )[0];
+	if ( d.getElementById( id ) ) {return;}
+	e = d.createElement( s ); e.id = id;
+	e.src = 'https://cdn.siftscience.com/s.js';
+	f.parentNode.insertBefore( e, f );
+})( document, 'script', 'sift-beacon' );
