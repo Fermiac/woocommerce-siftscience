fab4f5193adfd51fa177aef68dcd8c3f6f9d5eba
Nabeel Sulieman <nabsul@outlook.com>
2016-05-08 09:48:15 -0400

    Make the upload button green

--- app/components/batch-upload/index.js
+++ app/components/batch-upload/index.js
@@ -9,8 +9,12 @@ const component = () => {
 						<label>Batch Upload</label>
 					</th>
 					<td class="forminp forminp-text">
-						<button type="button" className="button-primary">Upload</button>
-						<p class="description">Send all your orders to SiftScience</p>
+						<button type="button" className="button-primary" style={ { backgroundColor: 'green' } }>
+							Upload
+						</button>
+						<p class="description">
+							Send all your orders to SiftScience
+						</p>
 					</td>
 				</tr>
 			</tbody>
