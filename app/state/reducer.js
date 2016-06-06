bb268b37b80c65203fe1636c4de803270be2fbdf
Nabeel Sulieman <nabeel.sulieman@live.com>
2016-06-06 08:25:40 -0700

    Add user management state Add delete actions for users and orders

--- app/state/reducer.js
+++ app/state/reducer.js
@@ -9,6 +9,35 @@ actionMap[actionTypes.UPDATE_ORDER] = ( state, action ) => {
 	return Object.assign( {}, state, { orders: newOrders } );
 };
 
+actionMap[actionTypes.DELETE_ORDER] = ( state, action ) => {
+	const id = action.id;
+	if ( ! state.orders[id] ) {
+		return state;
+	}
+
+	const newOrders = Object.assign( {}, state.orders );
+	delete newOrders[id];
+	return Object.assign( {}, state, { orders: newOrders } );
+};
+
+actionMap[actionTypes.UPDATE_USER] = ( state, action ) => {
+	const oldUser = ( state.users && state.users[action.id] ) ? state.users[action.id] : {};
+	const newUser = Object.assign( {}, oldUser, action.value );
+	const newUsers = Object.assign( {}, state.users, { [action.id]: newUser } );
+	return Object.assign( {}, state, { users: newUsers } );
+};
+
+actionMap[actionTypes.DELETE_USER] = ( state, action ) => {
+	const id = action.id;
+	if ( ! state.users[id] ) {
+		return state;
+	}
+
+	const newUsers = Object.assign( {}, state.users );
+	delete newUsers[id];
+	return Object.assign( {}, state, { users: newUsers } );
+};
+
 actionMap[actionTypes.UPDATE_BATCH] = ( state, action ) => {
 	const oldBatch = state.batch || {};
 	const newBatch = Object.assign( {}, oldBatch, action.value )
