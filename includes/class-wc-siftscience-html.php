680fc37603e0b676f983b99daab717ba9352f292
Nabeel Sulieman <nabeel.sulieman@live.com>
2016-06-18 18:50:42 -0700

    Remove unused code from the html class

--- includes/class-wc-siftscience-html.php
+++ includes/class-wc-siftscience-html.php
@@ -22,23 +22,6 @@ if ( ! class_exists( 'WC_SiftScience_Html' ) ) :
 			return "<span style='display: block;' class='tips' data-tip='$text'>$inner</span>";
 		}
 
-		public static function score( $value ) {
-			$color = $value > 90 ? 'red' : ( $value > 80 ? 'orange' : 'green' );
-
-			$style = self::css_style( array(
-				'color'            => 'white',
-				'text-align'       => 'center',
-				'background-color' => $color,
-				'border'           => '1px solid black',
-				'width'            => '20px',
-				'height'           => '20px',
-				'margin'           => '0px',
-				//'display' => 'inline',
-			) );
-
-			return "<div style='$style'>$value</div>";
-		}
-
 		public static function div( $content, $params ) {
 			$divTags = self::tag_params( $params );
 			return "<div$divTags>$content</div>";
@@ -50,61 +33,6 @@ if ( ! class_exists( 'WC_SiftScience_Html' ) ) :
 				$result .= " $k=$strEnclose$v$strEnclose";
 			}
 			return $result;
-		}
-
-		private static function css_style( $properties ) {
-			$style = '';
-			foreach ( $properties as $k => $v ) {
-				$style .= "$k: $v;";
-			}
-			return $style;
-		}
-
-		public static function score_label_icons( $vertical = true ) {
-			global $post;
-			$id = $post->ID;
-			$hide = 'none';
-			$float = $vertical ? 'none' : 'left';
-			echo( $vertical ? '' : '<div style="float: inline; height: 22px;">' );
-			echo self::create_icon_div( $id, 'siftsci_error', 'get', 'error.png', 'error', 'Something went wrong. Click to try again.', $hide, $float );
-			echo self::create_score_div( $id, 'siftsci_score', 99, 'Click to go to SiftScience page.', $hide, $float );
-			echo self::create_icon_div( $id, 'siftsci_backfill', 'backfill', 'upload.png', 'backfill', 'Click to send info to SiftScience...', $hide, $float );
-			echo self::create_icon_div( $id, 'siftsci_good_gray', 'set_good', 'good-gray.png', 'good', 'Click to mark as good...', $hide, $float );
-			echo self::create_icon_div( $id, 'siftsci_good', 'unset', 'good.png', 'good', 'Click to undo mark as good...', $hide, $float );
-			echo self::create_icon_div( $id, 'siftsci_bad_gray', 'set_bad', 'bad-gray.png', 'bad', 'Click to mark as bad...', $hide, $float );
-			echo self::create_icon_div( $id, 'siftsci_bad', 'unset', 'bad.png', 'bad', 'Click to undo mark as bad...', $hide, $float );
-			echo self::create_icon_div( $id, 'siftsci_spinner', 'none', 'spinner.gif', 'loading', 'Loading SiftScience Data...', 'block', $float );
-			echo( $vertical ? '' : '</div>' );
-		}
-
-		private static function create_score_div( $id, $pre, $val, $tt, $visible, $float ) {
-			$score = WC_SiftScience_Html::score( $val );
-			$content = WC_SiftScience_Html::tool_tip( $score, $tt );
-			$meta = get_post_meta( $id );
-			$user = urlencode( $meta['_customer_user'][0] );
-			$url = "https://siftscience.com/console/users/" . urlencode( $user );
-			$tagParams = array(
-				'id'      => "{$pre}_$id",
-				'onclick' => "window.open('$url');",
-				'class'   => $pre,
-				'style'   => "width: 24px; float: $float; display: $visible;",
-			);
-
-			return WC_SiftScience_Html::div( $content, $tagParams );
-		}
-
-		private static function create_icon_div( $id, $pre, $action, $img, $alt, $tt, $visible = 'block', $float = 'none' ) {
-			$icon = WC_SiftScience_Html::icon( $img, $alt );
-			$content = WC_SiftScience_Html::tool_tip( $icon, $tt );
-
-			$tagParams = array(
-				'id'      => "{$pre}_$id",
-				'onclick' => "SiftScienceOrder.callApi($id,'$action');",
-				'class'   => $pre,
-				'style'   => "width: 24px; display: $visible; float: $float;",
-			);
-
-			return WC_SiftScience_Html::div( $content, $tagParams );
 		}
 
 		public static function enqueue_script( $scriptName, $data = null ) {
