93676b8dfa45421c83af3c15969e3fff55a16b26
Nabeel Sulieman <nabeel.sulieman@live.com>
2016-10-08 09:39:48 -0700

    Add a webpack command that produces a smaller file

---
+++ webpack.production.config.js
@@ -0,0 +1,22 @@
+const webpack = require( 'webpack' );
+const config = require( './webpack.config.js' );
+
+delete config.output.publicPath;
+delete config.devtool;
+
+config.plugins.push( new webpack.DefinePlugin( {
+	'process.env.NODE_ENV': '"production"'
+} ) );
+
+config.plugins.push( new webpack.optimize.OccurrenceOrderPlugin( false ) );
+
+config.plugins.push( new webpack.optimize.UglifyJsPlugin( {
+	compress: {
+		warnings: false,
+		unsafe: true,
+	},
+} ) );
+
+config.plugins.push( new webpack.optimize.DedupePlugin() );
+
+module.exports = config;
