cba4642ca2689ee34f5a41f2170ed8b1f1654c67
Nabeel Sulieman <nabeel.sulieman@live.com>
2016-11-13 11:47:14 -0800

    Improve error handling of the api call with better logging and reporting

--- app/lib/api/index.js
+++ app/lib/api/index.js
@@ -3,26 +3,33 @@ import settings from '../settings';
 const fetchApi = ( action, id, callback ) => {
 	const idString = id ? '&id=' + id : '';
 	const url = settings.apiUrl + '?action=' + action + idString;
-	let isError = false;
-	fetch( url, {
-		credentials: 'same-origin',
-	} ).then( res => {
-		if ( 200 > res.status || 300 < res.status ) {
-			isError = true;
-		}
+	let text = null;
+	fetch( url, { credentials: 'same-origin' } )
+		.then( ( res ) => {
+			if ( 200 > res.status || 300 < res.status ) {
+				return res.text()
+					.then( ( textResult ) => {
+						text = textResult;
+						const error = new Error( 'Server Error' );
+						error.text = textResult;
+						throw error;
+					} )
+			}
 
-		return res.json();
-	} ).then( json => {
-		if ( ! isError ) {
-			return callback( null, json );
-		}
-
-		const error = new Error( 'Server Error' );
-		error.text = json.error;
-		callback( error );
-	} ).catch( error => {
-		callback( error );
-	} );
+			return res.text();
+		} )
+		.then( ( textResult ) => {
+			text = textResult;
+			return JSON.parse( text )
+		} )
+		.then( ( json ) => callback( null, json ) )
+		.catch( ( error ) => {
+			console.log( 'error parsing api result' );
+			console.log( error );
+			console.log( 'api returned: ' );
+			console.log( text );
+			callback( error );
+		} );
 };
 
 export default fetchApi;
