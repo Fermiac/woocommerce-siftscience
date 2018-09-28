ec04dc63294ffab19f786108fd9c347e459bd696
Nabeel Sulieman <nabsul@outlook.com>
2018-09-28 10:51:01 -0700

    Use get_id() for order_id instead of accessing the member directly

--- includes/third-party/class-wc-siftscience-stripe.php
+++ includes/third-party/class-wc-siftscience-stripe.php
@@ -45,11 +45,11 @@ class WC_SiftScience_Stripe {
 			'$card_last4'      => $source->last4,
 			'$cvv_result_code' => $source->cvc_check,
 			'$stripe_address_line1_check' => $source->address_line1_check,
-			'$stripe_address_zip_check'   => $source->address_zip_check
+			'$stripe_address_zip_check'   => $source->address_zip_check,
 		);
 
 		$data = array( 'payment_method' => $payment_details );
-		update_post_meta( $order->id, self::$order_data_key, json_encode( $data ) );
+		update_post_meta( $order->get_id(), self::$order_data_key, json_encode( $data ) );
 	}
 
 	public function order_payment_method( $current_method, WC_Order $order ) {
@@ -66,7 +66,7 @@ class WC_SiftScience_Stripe {
 	}
 
 	private function get_order_meta( WC_Order $order ) {
-		$meta = get_post_meta( $order->id, self::$order_data_key, true );
+		$meta = get_post_meta( $order->get_id(), self::$order_data_key, true );
 		if ( ! is_string( $meta ) || 0 === strlen( $meta ) ) {
 			return null;
 		}
