7afd66a6a3e3d254a278d0a7ed7c6e39c4444bd2
ramico <ramico@gmail.com>
2020-07-22 16:56:33 +0200

    get_order_currency
    get_currency

--- includes/class-wc-siftscience-format-transaction.php
+++ includes/class-wc-siftscience-format-transaction.php
@@ -30,7 +30,7 @@ if ( ! class_exists( 'WC_SiftScience_Format_Transaction' ) ) :
 				'$session_id'         => $this->options->get_order_session_id( $order ),
 				'$order_id'           => $order->get_order_number(),
 				'$amount'             => $order->get_total() * 1000000,
-				'$currency_code'      => $order->get_order_currency(),
+				'$currency_code'      => $order->get_currency(),
 				'$transaction_type'   => $this->get_transaction_type( $order ),
 				'$transaction_status' => $this->get_transaction_status( $order ),
 				'$payment_method'     => $this->get_payment_method( $order ),
