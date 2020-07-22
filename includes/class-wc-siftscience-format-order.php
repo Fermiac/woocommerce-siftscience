c74391d0a04de99c8c8dd93555cee0f87a12badb
ramico <ramico@gmail.com>
2020-07-22 23:14:38 +0200

    shipping_state
    get_shipping_state()

--- includes/class-wc-siftscience-format-order.php
+++ includes/class-wc-siftscience-format-order.php
@@ -159,7 +159,7 @@ if ( ! class_exists( 'WC_SiftScience_Format_Order' ) ) :
 					'$address_1' => $order->get_shipping_address_1(),
 					'$address_2' => $order->get_shipping_address_2(),
 					'$city'      => $order->get_shipping_city(),
-					'$region'    => $this->get_order_param( $order, $type, '_state' ),
+					'$region'    => $order->get_shipping_state(),
 					'$country'   => $this->get_order_param( $order, $type, '_country' ),
 					'$zipcode'   => $this->get_order_param( $order, $type, '_postcode' ),
 				);
