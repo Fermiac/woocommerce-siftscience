f0626134a29656cbb411fe8810f08a37c6a88d13
ramico <ramico@gmail.com>
2020-07-26 17:22:06 +0200

    making checkbox text adjacent
    c the image

--- includes/class-wc-siftscience-admin.php
+++ includes/class-wc-siftscience-admin.php
@@ -76,6 +76,9 @@ if ( ! class_exists( 'WC_SiftScience_Admin' ) ) :
 
 		private function output_settings_main() {
 			WC_Admin_Settings::output_fields( $this->get_settings() );
+			
+			//important code fragment Rjamleh
+			echo '<style type="text/css">label[for="'.WC_SiftScience_Options::$send_on_create_enabled.'"]+p{display:inline-block;vertical-align:middle;}</style>';
 
 			echo $this->batch_upload();
 			$data = array( 'api' => admin_url( 'admin-ajax.php' ) );
