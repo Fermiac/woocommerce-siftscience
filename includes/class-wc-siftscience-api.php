b23ce82aad9f730ac5cd2e329dcd2ec484688969
ramico <ramico@gmail.com>
2020-08-08 22:44:10 +0200

    api class file
    underscore added

--- includes/class-wc-siftscience-api.php
+++ includes/class-wc-siftscience-api.php
@@ -18,19 +18,19 @@ if ( ! class_exists( "WC_SiftScience_Api" ) ) :
 	require_once( 'class-wc-siftscience-stats.php' );
 
 	class WC_SiftScience_Api {
-		private $comm;
-		private $events;
-		private $options;
-		private $logger;
-		private $stats;
+		private $_comm;
+		private $_events;
+		private $_options;
+		private $_logger;
+		private $_stats;
 
 		public function __construct( WC_SiftScience_Comm $comm, WC_SiftScience_Events $events,
 			WC_SiftScience_Options $options, WC_SiftScience_Logger $logger, WC_SiftScience_Stats $stats ) {
-			$this->comm = $comm;
-			$this->events = $events;
-			$this->options = $options;
-			$this->logger = $logger;
-			$this->stats = $stats;
+			$this->_comm = $comm;
+			$this->_events = $events;
+			$this->_options = $options;
+			$this->_logger = $logger;
+			$this->_stats = $stats;
 		}
 
 		public function handle_ajax() {
@@ -44,11 +44,11 @@ if ( ! class_exists( "WC_SiftScience_Api" ) ) :
 				}
 
 				$response = json_encode( $result );
-				$this->logger->log_info( '[ajax response] ' . $response );
+				$this->_logger->log_info( '[ajax response] ' . $response );
 				echo $response;
 			} catch ( Exception $error ) {
-				$this->logger->log_exception( $error );
-				$this->stats->send_error( $error );
+				$this->_logger->log_exception( $error );
+				$this->_stats->send_error( $error );
 				http_response_code( 500 );
 				echo json_encode( array(
 					'error' => true,
@@ -83,18 +83,18 @@ if ( ! class_exists( "WC_SiftScience_Api" ) ) :
 				case 'score':
 					break;
 				case 'set_good':
-					$this->comm->post_label( $user_id, false );
+					$this->_comm->post_label( $user_id, false );
 					break;
 				case 'set_bad':
-					$this->comm->post_label( $user_id, true );
+					$this->_comm->post_label( $user_id, true );
 					break;
 				case 'unset':
-					$this->comm->delete_label( $user_id );
+					$this->_comm->delete_label( $user_id );
 					break;
 				case 'backfill':
-					$this->events->set_backfill( $order_id );
-					$this->events->create_order( $order_id );
-					$this->events->send_queued_data();
+					$this->_events->set_backfill( $order_id );
+					$this->_events->create_order( $order_id );
+					$this->_events->send_queued_data();
 					break;
 				case 'order_stats':
 					return $this->list_stats();
@@ -121,7 +121,7 @@ if ( ! class_exists( "WC_SiftScience_Api" ) ) :
 		private function list_stats() {
 			$backfilled = array();
 			$not_backfilled = array();
-			$meta_key = $this->options->get_backfill_meta_key();
+			$meta_key = $this->_options->get_backfill_meta_key();
 			$posts = $this->get_order_posts();
 			foreach( $posts as $post ) {
 				if ( '1' === get_post_meta( $post->ID, $meta_key, true ) ) {
@@ -138,7 +138,7 @@ if ( ! class_exists( "WC_SiftScience_Api" ) ) :
 		}
 
 		private function clear_all() {
-			$meta_key = $this->options->get_backfill_meta_key();
+			$meta_key = $this->_options->get_backfill_meta_key();
 			$posts = $this->get_order_posts();
 			foreach( $posts as $post ) {
 				delete_post_meta( $post->ID, $meta_key );
@@ -158,9 +158,9 @@ if ( ! class_exists( "WC_SiftScience_Api" ) ) :
 		}
 
 		private function get_score( $order_id, $user_id ) {
-			$backfill_meta_key = $this->options->get_backfill_meta_key();
+			$backfill_meta_key = $this->_options->get_backfill_meta_key();
 			$is_backfilled = get_post_meta( $order_id, $backfill_meta_key, true ) === '1';
-			$sift = $this->comm->get_user_score( $user_id );
+			$sift = $this->_comm->get_user_score( $user_id );
 
 			return array(
 				'order_id' => $order_id,
@@ -181,8 +181,8 @@ if ( ! class_exists( "WC_SiftScience_Api" ) ) :
 			}
 
 			return $meta === '0'
-				? $this->options->get_user_id_from_order_id( $order_id )
-				: $this->options->get_user_id_from_user_id( $meta );
+				? $this->_options->get_user_id_from_order_id( $order_id )
+				: $this->_options->get_user_id_from_user_id( $meta );
 		}
 	}
 
