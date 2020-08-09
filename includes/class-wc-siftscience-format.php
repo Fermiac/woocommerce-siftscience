4216df2b92d47f0e5396314ca2660123b293b19c
ramico <ramico@gmail.com>
2020-08-09 02:05:29 +0200

    format.php file

--- includes/class-wc-siftscience-format.php
+++ includes/class-wc-siftscience-format.php
@@ -22,20 +22,20 @@ if ( ! class_exists( 'WC_SiftScience_Format' ) ) :
 	require_once( 'class-wc-siftscience-format-transaction.php' );
 
 	class WC_SiftScience_Format {
-		public $items;
-		public $login;
-		public $account;
-		public $order;
-		public $cart;
-		public $transactions;
+		public $_items;
+		public $_login;
+		public $_account;
+		public $_order;
+		public $_cart;
+		public $_transactions;
 
 		public function __construct( WC_SiftScience_Options $options, WC_SiftScience_Logger $logger ) {
-			$this->transactions = new WC_SiftScience_Format_Transaction( $options );
-			$this->items = new WC_SiftScience_Format_Items( $options );
-			$this->login = new WC_SiftScience_Format_Login( $options );
-			$this->account = new WC_SiftScience_Format_Account( $options );
-			$this->order = new WC_SiftScience_Format_Order( $this->items, $this->transactions, $options, $logger );
-			$this->cart = new WC_SiftScience_Format_Cart( $options );
+			$this->_transactions = new WC_SiftScience_Format_Transaction( $options );
+			$this->_items = new WC_SiftScience_Format_Items( $options );
+			$this->_login = new WC_SiftScience_Format_Login( $options );
+			$this->_account = new WC_SiftScience_Format_Account( $options );
+			$this->_order = new WC_SiftScience_Format_Order( $this->_items, $this->_transactions, $options, $logger );
+			$this->_cart = new WC_SiftScience_Format_Cart( $options );
 		}
 	}
 
