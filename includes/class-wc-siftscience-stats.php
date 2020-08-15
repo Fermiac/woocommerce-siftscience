02adee0dc2fe4a5a7d15086f9e47788e04bb9aac
Nabeel Sulieman <nabsul@outlook.com>
2020-08-15 15:20:51 -0700

    Fix all warnings in stats file

--- includes/class-wc-siftscience-stats.php
+++ includes/class-wc-siftscience-stats.php
@@ -1,42 +1,94 @@
 <?php
-
-/*
+/**
  * Author: Nabeel Sulieman
  * Description: This class handles sending anonymous stats and error messages
- * License: GPL2
+
+ * @package WC_SiftScience_Stats
+ * @class WC_SiftScience_Stats
+ * @license GPL2
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-if ( ! class_exists( "WC_SiftScience_Stats" ) ) :
+if ( ! class_exists( 'WC_SiftScience_Stats' ) ) :
 
+	/**
+	 * This class handles stats collection and transmission
+	 *
+	 * Class WC_SiftScience_Stats
+	 */
 	class WC_SiftScience_Stats {
-		private $_logger;
-		private $_options;
-		private $_stats;
-		private $_last_sent;
-		private $_send_period;
+		/**
+		 * The logging object
+		 *
+		 * @var WC_SiftScience_Logger
+		 */
+		private $logger;
 
+		/**
+		 * The options object
+		 *
+		 * @var WC_SiftScience_Options
+		 */
+		private $options;
+
+		/**
+		 * Cache of stats collected
+		 *
+		 * @var array
+		 */
+		private $stats;
+
+		/**
+		 * Time last sent
+		 *
+		 * @var int
+		 */
+		private $last_sent;
+
+		/**
+		 * Frequency to send data
+		 *
+		 * @var int
+		 */
+		private $send_period;
+
+		/**
+		 * WC_SiftScience_Stats constructor.
+		 *
+		 * @param WC_SiftScience_Options $options Options class.
+		 * @param WC_SiftScience_Logger  $logger Logger class.
+		 */
 		public function __construct( WC_SiftScience_Options $options, WC_SiftScience_Logger $logger ) {
-			$this->_options   = $options;
-			$this->_logger    = $logger;
-			$this->_last_sent = get_option( WC_SiftScience_Options::STATS_LAST_SENT, 0 );
+			$this->options   = $options;
+			$this->logger    = $logger;
+			$this->last_sent = get_option( WC_SiftScience_Options::STATS_LAST_SENT, 0 );
 
 			$stats       = get_option( WC_SiftScience_Options::STATS, false );
-			$this->_stats = ( $stats === false ) ? array() : json_decode( $stats, true );
+			$this->stats = ( false === $stats ) ? array() : json_decode( $stats, true );
 			if ( defined( 'WP_SIFTSCI_STATS_PERIOD' ) ) {
-				$this->_send_period = WP_SIFTSCI_STATS_PERIOD;
+				$this->send_period = WP_SIFTSCI_STATS_PERIOD;
 			} else {
-				$this->_send_period = 60 * 60; //send stats once every hour at most.
+				$this->send_period = 60 * 60; // send stats once every hour at most.
 			}
 		}
 
+		/**
+		 * Clear the stats object
+		 */
 		public function clear_stats() {
-			$this->_stats = array();
+			$this->stats = array();
 		}
 
+		/**
+		 * Create a timer to track send time
+		 *
+		 * @param string $metric Name of metric to time.
+		 *
+		 * @return array
+		 */
 		public function create_timer( $metric ) {
 			return array(
 				'name'  => $metric,
@@ -44,77 +96,106 @@ if ( ! class_exists( "WC_SiftScience_Stats" ) ) :
 			);
 		}
 
+		/**
+		 * Save the stats timer
+		 *
+		 * @param array $timer Value of the timer to be saved.
+		 */
 		public function save_timer( $timer ) {
-			$metric       = $timer[ 'name' ];
-			$start_time   = $timer[ 'start' ];
+			$metric       = $timer['name'];
+			$start_time   = $timer['start'];
 			$current_time = microtime( true );
 			$time         = $current_time - $start_time;
 
-			if ( ! isset( $this->_stats[ $metric ] ) ) {
-				$this->_stats[ $metric ] = array();
+			if ( ! isset( $this->stats[ $metric ] ) ) {
+				$this->stats[ $metric ] = array();
 			}
 
-			if ( ! isset( $this->_stats[ $metric ][ 'count' ] ) ) {
-				$this->_stats[ $metric ][ 'count' ] = 0;
+			if ( ! isset( $this->stats[ $metric ]['count'] ) ) {
+				$this->stats[ $metric ]['count'] = 0;
 			}
 
-			if ( ! isset( $this->_stats[ $metric ][ 'time' ] ) ) {
-				$this->_stats[ $metric ][ 'time' ] = 0;
+			if ( ! isset( $this->stats[ $metric ]['time'] ) ) {
+				$this->stats[ $metric ]['time'] = 0;
 			}
 
-			$this->_stats[ $metric ][ 'time' ] += $time;
-			$this->_stats[ $metric ][ 'count' ] += 1;
+			$this->stats[ $metric ]['time']  += $time;
+			$this->stats[ $metric ]['count'] += 1;
 		}
 
+		/**
+		 * Send or save all stats before destroying this object
+		 */
 		public function shutdown() {
 			$this->send_stats();
 			$this->save_stats();
 		}
 
+		/**
+		 * Saves all stats
+		 */
 		private function save_stats() {
-			$stats = json_encode( $this->_stats );
+			$stats = wp_json_encode( $this->stats );
 			update_option( WC_SiftScience_Options::STATS, $stats );
 		}
 
+		/**
+		 * Send an error to stats collection
+		 *
+		 * @param Exception $error The error that occurred.
+		 */
 		public function send_error( Exception $error ) {
 			if ( ! $this->is_reporting_enabled() ) {
 				return;
 			}
 
 			$data = array(
-				'guid'  => $this->_options->get_guid(),
+				'guid'  => $this->options->get_guid(),
 				'type'  => 'error',
 				'error' => $error->__toString(),
 			);
 			$this->send_data( $data );
 		}
 
+		/**
+		 * Send all stats
+		 */
 		private function send_stats() {
 			if ( ! ( $this->is_reporting_enabled() && $this->is_time_to_send() ) ) {
 				return;
 			}
 
 			$data = array(
-				'guid'    => $this->_options->get_guid(),
+				'guid'    => $this->options->get_guid(),
 				'type'    => 'stats',
-				'version' => $this->_options->get_version(),
-				'stats'   => $this->_stats,
+				'version' => $this->options->get_version(),
+				'stats'   => $this->stats,
 			);
 
 			update_option( WC_SiftScience_Options::STATS_LAST_SENT, microtime( true ) );
 			$this->send_data( $data );
 		}
 
+		/**
+		 * Attempt to send out data, wrapped in a try-catch
+		 *
+		 * @param array $data The data to be sent.
+		 */
 		private function send_data( $data ) {
 			try {
 				$timer = $this->create_timer( 'stats_send_data' );
 				$this->send_data_inner( $data );
 				$this->save_timer( $timer );
 			} catch ( Exception $exception ) {
-				$this->_logger->log_exception( $exception );
+				$this->logger->log_exception( $exception );
 			}
 		}
 
+		/**
+		 * Inner loop for sending data
+		 *
+		 * @param array $data The data to be sent.
+		 */
 		private function send_data_inner( $data ) {
 			$url = WC_SiftScience_Options::STATS_API;
 
@@ -126,30 +207,40 @@ if ( ! class_exists( "WC_SiftScience_Stats" ) ) :
 			$request = array(
 				'headers' => $headers,
 				'method'  => 'POST',
-				'body'    => json_encode( $data ),
+				'body'    => wp_json_encode( $data ),
 				'timeout' => 5,
 			);
 
 			$result = wp_remote_request( $url, $request );
 
 			$is_success = isset( $result )
-			              && ! is_wp_error( $result )
-			              && isset( $result[ 'response' ] )
-			              && isset( $result[ 'response' ][ 'code' ] )
-			              && 200 === $result[ 'response' ][ 'code' ];
+				&& ! is_wp_error( $result )
+				&& isset( $result['response'] )
+				&& isset( $result['response']['code'] )
+				&& 200 === $result['response']['code'];
 
 			if ( ! $is_success ) {
-				$this->_logger->log_error( 'Failed to send stats' );
-				$this->_logger->log_error( $result );
+				$this->logger->log_error( 'Failed to send stats' );
+				$this->logger->log_error( $result );
 			}
 		}
 
+		/**
+		 * Check if reporting is enabled in options
+		 *
+		 * @return bool True if reporting is enabled
+		 */
 		private function is_reporting_enabled() {
 			return 'yes' === get_option( WC_SiftScience_Options::SEND_STATS, 'no' );
 		}
 
+		/**
+		 * Check if it's time to send stats
+		 *
+		 * @return bool True if it's time to send out stats
+		 */
 		private function is_time_to_send() {
-			return ( microtime( true ) - $this->_last_sent ) > $this->_send_period;
+			return ( microtime( true ) - $this->last_sent ) > $this->send_period;
 		}
 	}
 
