0683cea262cc2f3358fa626460cab628850f645f
ramico <ramico@gmail.com>
2020-08-17 00:53:29 +0200

    format_account_phpcs
    line: 65 get_user_by us a wridpress method which returns only FALSE when user ID not found
    
    if insisted empty shall be used
    
    WORDPRESS documentation:https://developer.wordpress.org/reference/functions/get_user_by/

--- includes/class-wc-siftscience-format-account.php
+++ includes/class-wc-siftscience-format-account.php
@@ -1,9 +1,11 @@
 <?php
 
-/*
- * Author: Nabeel Sulieman
- * Description: This class format woocommerce account events into the Sift format.
- * License: GPL2
+/**
+ * This class format woocommerce account events into the Sift format.
+ *
+ * @author Nabeel Sulieman, Rami Jamleh
+ * @package siftsience
+ * @license GPL2
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -15,20 +17,20 @@ if ( ! class_exists( 'WC_SiftScience_Format_Account' ) ) :
 	require_once 'class-wc-siftscience-options.php';
 
 	class WC_SiftScience_Format_Account {
-		private $_options;
+		private $options;
 
 		public function __construct( WC_SiftScience_Options $options ) {
-			$this->_options = $options;
+			$this->options = $options;
 		}
 
 		// https://sift.com/developers/docs/v204/curl/events-api/reserved-events/create-account.
 		public function create_account( $user_id, WP_User $user ) {
 			$data = array(
 				'$type'       => '$create_account',
-				'$user_id'    => $this->_options->get_user_id_from_user_id( $user_id ),
-				'$session_id'       => $this->_options->get_session_id(),
-				'$user_email'       => $user->user_email,
-				'$name'             => $user->first_name . ' ' . $user->last_name,
+				'$user_id'    => $this->options->get_user_id_from_user_id( $user_id ),
+				'$session_id' => $this->options->get_session_id(),
+				'$user_email' => $user->user_email,
+				'$name'       => $user->first_name . ' ' . $user->last_name,
 			);
 
 			return apply_filters( 'wc_siftscience_create_account', $data );
@@ -38,8 +40,8 @@ if ( ! class_exists( 'WC_SiftScience_Format_Account' ) ) :
 		public function update_account( $user_id, $old_user_data ) {
 			$user = get_userdata( $user_id );
 			$data = array(
-				'$type'       => '$update_account',
-				'$user_id'    => $this->_options->get_user_id_from_user_id( $user_id ),
+				'$type'             => '$update_account',
+				'$user_id'          => $this->options->get_user_id_from_user_id( $user_id ),
 				'$changed_password' => $this->is_password_changed( $user_id, $old_user_data ),
 				'$user_email'       => $user->user_email,
 				'$name'             => $user->first_name . ' ' . $user->last_name,
@@ -49,10 +51,10 @@ if ( ! class_exists( 'WC_SiftScience_Format_Account' ) ) :
 		}
 
 		public function link_session_to_user( $user_id ) {
-			$data = array (
+			$data = array(
 				'$type'       => '$link_session_to_user',
-				'$user_id'    => $this->_options->get_user_id_from_user_id( $user_id ),
-				'$session_id' => $this->_options->get_session_id(),
+				'$user_id'    => $this->options->get_user_id_from_user_id( $user_id ),
+				'$session_id' => $this->options->get_session_id(),
 			);
 
 			return apply_filters( 'wc_siftscience_link_session_to_user', $data );
@@ -60,8 +62,9 @@ if ( ! class_exists( 'WC_SiftScience_Format_Account' ) ) :
 
 		private function is_password_changed( $user_id, $old_user_data ) {
 			$user = get_user_by( 'id', $user_id );
-			if ( $user === null || $user === false || $old_user_data === null )
+			if ( false === $user || null === $old_user_data ) {
 				return false;
+			}
 			return ( isset( $old_user_data->user_pass ) && $user->user_pass !== $old_user_data->user_pass );
 		}
 	}
