4add92c703055966cb8da28a737313f0b018973e
Nabeel Sulieman <nabsul@users.noreply.github.com>
2020-08-15 21:08:32 -0700

    Get everything in Orders fixed except for the html escaping (#253)

--- includes/class-wc-siftscience-orders.php
+++ includes/class-wc-siftscience-orders.php
@@ -1,9 +1,10 @@
 <?php
-
-/*
+/**
  * Author: Nabeel Sulieman
  * Description: This class handles the display of Sift feedback icons in order list and order view.
- * License: GPL2
+ *
+ * @package WC_SiftScience_Orders
+ * @license GPL2
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -12,45 +13,75 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( ! class_exists( 'WC_SiftScience_Orders' ) ) :
 
-	require_once( 'class-wc-siftscience-html.php' );
-	require_once( 'class-wc-siftscience-options.php' );
+	require_once 'class-wc-siftscience-html.php';
+	require_once 'class-wc-siftscience-options.php';
 
+	/**
+	 * Class WC_SiftScience_Orders
+	 */
 	class WC_SiftScience_Orders {
-		private $_options;
+		/**
+		 * The options service
+		 *
+		 * @var WC_SiftScience_Options
+		 */
+		private $options;
 
+		/**
+		 * WC_SiftScience_Orders constructor.
+		 *
+		 * @param WC_SiftScience_Options $options The options service.
+		 */
 		public function __construct( WC_SiftScience_Options $options ) {
-			$this->_options = $options;
+			$this->options = $options;
 		}
 
+		/**
+		 * Outputs the html to the row in orders list page
+		 *
+		 * @param string $column The current column name.
+		 */
 		public function create_row( $column ) {
-			if ( $column == 'siftsci' ) {
+			if ( 'siftsci' === $column ) {
 				global $post;
 				$id = $post->ID;
 				echo "<div class='siftsci-order' id='siftsci-order-$id' data-id='$id'></div>\n";
 			}
 		}
 
+		/**
+		 * Adds the VueJS app for the order control
+		 * TODO: Rename this function
+		 */
 		private function add_react_app() {
 			$data = array(
-				'imgPath' => plugins_url( 'images/', dirname( __FILE__ ) ),
-				'api' => admin_url( 'admin-ajax.php' ),
-				'thresholdGood' => $this->_options->get_threshold_good(),
-				'thresholdBad' => $this->_options->get_threshold_bad(),
+				'imgPath'       => plugins_url( 'images/', dirname( __FILE__ ) ),
+				'api'           => admin_url( 'admin-ajax.php' ),
+				'thresholdGood' => $this->options->get_threshold_good(),
+				'thresholdBad'  => $this->options->get_threshold_bad(),
 			);
 
-			wp_enqueue_script( 'wc-siftsci-vuejs', plugins_url( "dist/vue-dev.js", dirname( __FILE__ ) ), array(), time(), true );
-			wp_enqueue_script( 'wc-siftsci-control', plugins_url( "dist/OrderControl.umd.js", dirname( __FILE__ ) ), array('wc-siftsci-vuejs'), time(), true );
-			wp_enqueue_script( 'wc-siftsci-script', plugins_url( "dist/order-control.js", dirname( __FILE__ ) ), array('wc-siftsci-control'), time(), true );
-			wp_localize_script( 'wc-siftsci-script', "_siftsci_app_data", $data );
+			wp_enqueue_script( 'wc-siftsci-vuejs', plugins_url( 'dist/vue-dev.js', dirname( __FILE__ ) ), array(), time(), true );
+			wp_enqueue_script( 'wc-siftsci-control', plugins_url( 'dist/OrderControl.umd.js', dirname( __FILE__ ) ), array( 'wc-siftsci-vuejs' ), time(), true );
+			wp_enqueue_script( 'wc-siftsci-script', plugins_url( 'dist/order-control.js', dirname( __FILE__ ) ), array( 'wc-siftsci-control' ), time(), true );
+			wp_localize_script( 'wc-siftsci-script', '_siftsci_app_data', $data );
 		}
 
+		/**
+		 * Adds the header for the column on the orders list page.
+		 * The header is added after the order_status column
+		 *
+		 * @param array $columns The columns list to be filtered.
+		 *
+		 * @return array The new list of colums with the new column added
+		 */
 		public function create_header( $columns ) {
 			$newcolumns = array();
 
 			foreach ( $columns as $k => $v ) {
-				$newcolumns[$k] = $v;
+				$newcolumns[ $k ] = $v;
 
-				if ( $k == 'order_status' ) {
+				if ( 'order_status' === $k ) {
 					$newcolumns['siftsci'] = 'Sift';
 				}
 			}
@@ -60,6 +91,9 @@ if ( ! class_exists( 'WC_SiftScience_Orders' ) ) :
 			return $newcolumns;
 		}
 
+		/**
+		 * Adds a meta box on the orders page
+		 */
 		public function add_meta_box() {
 			add_meta_box(
 				'wc_sift_score_meta_box',
@@ -71,6 +105,9 @@ if ( ! class_exists( 'WC_SiftScience_Orders' ) ) :
 			);
 		}
 
+		/**
+		 * Display the siftscience div for the VueJS control
+		 */
 		public function display_siftsci_box() {
 			global $post;
 			$id = $post->ID;
