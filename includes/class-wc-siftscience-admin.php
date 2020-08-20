f474cf9409e52ccc93f8138154e6afcc1449c97a
ramico <ramico@gmail.com>
2020-08-20 16:53:29 +0200

    sibce besting and order is not required
    this makes perfect sense

--- includes/class-wc-siftscience-admin.php
+++ includes/class-wc-siftscience-admin.php
@@ -24,25 +24,25 @@ if ( ! class_exists( 'WC_SiftScience_Admin' ) ) :
 		private const ADMIN_LABEL = 'Sift';
 
 		private const ALLOWED_HTML = array(
-			'style' => array( 'type' => array() ),
-			'ul'    => array( 'class' => array() ),
 			'li'    => array(),
+			'table' => array(),
+			'tbody' => array(),
+			'thead' => array(),
+			'tr'    => array(),
+			'br'    => array( 'class' => array() ),
+			'span'  => array( 'style' => array() ),
+			'style' => array( 'type' => array() ),
+			'td'    => array( 'style' => array() ),
+			'ul'    => array( 'class' => array() ),
 			'a'     => array(
 				'href'  => array(),
 				'class' => array(),
 			),
-			'br'    => array( 'class' => array() ),
-			'span'  => array( 'style' => array() ),
-			'table' => array(),
-			'thead' => array(),
 			'th'    => array(
 				'scope'   => array(),
 				'colspan' => array(),
 				'style'   => array(),
 			),
-			'tbody' => array(),
-			'td'    => array( 'style' => array() ),
-			'tr'    => array(),
 		);
 
 		/**
