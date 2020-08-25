11af6c85ca2dc85a57d388f11f7e60f55ee36d1d
Nabeel Sulieman <nabsul@users.noreply.github.com>
2020-08-25 13:28:06 -0700

    Factor out the common get-var prefix into a constant (#296)

--- includes/class-wc-siftscience-admin.php
+++ includes/class-wc-siftscience-admin.php
@@ -20,14 +20,15 @@ if ( ! class_exists( 'WC_SiftScience_Admin' ) ) :
 	 * Class WC_SiftScience_Admin
 	 */
 	class WC_SiftScience_Admin {
-		private const ADMIN_ID            = 'siftsci';
-		private const ADMIN_LABEL         = 'Sift';
-		private const GET_VAR_SET_STATS   = 'wc_sift_send_stats';
-		private const GET_VAR_CLEAR_STATS = 'wc_sift_clear_stats';
-		private const GET_VAR_RESET_GUID  = 'wc_sift_reset_guid';
-		private const GET_VAR_TEST_SSL    = 'wc_sift_test_ssl';
-		private const GET_VAR_CLEAR_LOGS  = 'wc_sift_clear_logs';
-		private const NONCE_ACTION_PREFIX = 'nonce_action_';
+		private const ADMIN_ID    = 'siftsci';
+		private const ADMIN_LABEL = 'Sift';
+
+		private const GET_VAR_SCHEMA      = 'wc_sift_';
+		private const GET_VAR_SET_STATS   = self::GET_VAR_SCHEMA . 'send_stats';
+		private const GET_VAR_CLEAR_STATS = self::GET_VAR_SCHEMA . 'clear_stats';
+		private const GET_VAR_RESET_GUID  = self::GET_VAR_SCHEMA . 'reset_guid';
+		private const GET_VAR_TEST_SSL    = self::GET_VAR_SCHEMA . 'test_ssl';
+		private const GET_VAR_CLEAR_LOGS  = self::GET_VAR_SCHEMA . 'clear_logs';
 
 		private const ALLOWED_HTML = array(
 			'li'    => array(),
@@ -616,7 +617,7 @@ IMPROVE;
 
 			// Check that nonce is valid and input value exists.
 			$is_valid_input = isset( $_GET[ $var_name ], $_GET[ $nonce_name ] )
-				&& wp_verify_nonce( sanitize_key( $_GET[ $nonce_name ] ), $this->get_action_name( $var_name ) );
+				&& wp_verify_nonce( sanitize_key( $_GET[ $nonce_name ] ), $this->get_nonce_name( $var_name ) );
 
 			if ( false === $is_valid_input ) {
 				return false;
@@ -637,17 +638,6 @@ IMPROVE;
 		}
 
 		/**
-		 * This function auto-generates the nonce action name based on the get variable name
-		 *
-		 * @param String $get_var the GET array variable.
-		 *
-		 * @return String concatenated nonce name.
-		 */
-		private function get_action_name( $get_var ) {
-			return self::NONCE_ACTION_PREFIX . $get_var;
-		}
-
-		/**
 		 * Creates a variable URL with it's nonce respectivly
 		 *
 		 * @param String $get_var_name  the GET variable.
@@ -656,9 +646,10 @@ IMPROVE;
 		 * @return String bounded link with a get var and it's nonce.
 		 */
 		private function bound_nonce_url( $get_var_name, $get_var_value ) {
-			$url = add_query_arg( array( $get_var_name => $get_var_value ) );
-			$url = wp_nonce_url( $url, $this->get_action_name( $get_var_name ), $this->get_nonce_name( $get_var_name ) );
-			return $url;
+			$url        = add_query_arg( array( $get_var_name => $get_var_value ) );
+			$nonce_name = $this->get_nonce_name( $get_var_name );
+
+			return wp_nonce_url( $url, $nonce_name, $nonce_name );
 		}
 
 		/**
