8a356dd62c49f0fa7a6e8de6339e7b802be96bac
ramico <ramico@gmail.com>
2020-09-06 04:27:20 +0200

    schema revisited

--- includes/class-wc-siftsience-isift-options.php
+++ includes/class-wc-siftsience-isift-options.php
@@ -14,21 +14,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! interface_exists( 'Isift_Options' ) ) :
 	interface Isift_Options {
 
-		public const GUID    = 'siftsci_guid';
-		public const API_KEY = 'siftsci_api_key';
-		public const JS_KEY  = 'siftsci_js_key';
-		public const STATS   = 'siftsci_stats';
+		public const SCHEMA = 'siftsci_';
 
-		public const SEND_STATS  = 'siftsci_send_stats';
-		public const NAME_PREFIX = 'siftsci_name_prefix';
+		public const GUID    = self::SCHEMA . 'guid';
+		public const API_KEY = self::SCHEMA . 'api_key';
+		public const JS_KEY  = self::SCHEMA . 'js_key';
+		public const STATS   = self::SCHEMA . 'stats';
+
+		public const SEND_STATS  = self::SCHEMA . 'send_stats';
+		public const NAME_PREFIX = self::SCHEMA . 'name_prefix';
 		public const STATS_API   = 'https://sift.fermiac.staat.us';
 
-		public const THRESHOLD_GOOD    = 'siftsci_threshold_good';
-		public const THRESHOLD_BAD     = 'siftsci_threshold_bad';
-		public const AUTO_SEND_ENABLED = 'siftsci_auto_send_enabled';
-		public const MIN_ORDER_VALUE   = 'siftsci_min_order_value';
-		public const LOG_LEVEL_KEY     = 'siftsci_log_level';
-		public const IS_API_SETUP      = 'siftsci_is_api_setup';
-		public const STATS_LAST_SENT   = 'siftsci_stats_last_sent';
+		public const THRESHOLD_GOOD    = self::SCHEMA . 'threshold_good';
+		public const THRESHOLD_BAD     = self::SCHEMA . 'threshold_bad';
+		public const AUTO_SEND_ENABLED = self::SCHEMA . 'auto_send_enabled';
+		public const MIN_ORDER_VALUE   = self::SCHEMA . 'min_order_value';
+		public const LOG_LEVEL_KEY     = self::SCHEMA . 'log_level';
+		public const IS_API_SETUP      = self::SCHEMA . 'is_api_setup';
+		public const STATS_LAST_SENT   = self::SCHEMA . 'stats_last_sent';
 	}
 endif;
