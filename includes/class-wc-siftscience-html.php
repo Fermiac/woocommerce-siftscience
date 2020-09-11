683c6d48199310d1be48e5b789dce70f356c92d6
ramico <ramico@gmail.com>
2020-09-11 03:10:47 +0300

    move create_element to html class and add some validation (#320)

--- includes/class-wc-siftscience-html.php
+++ includes/class-wc-siftscience-html.php
@@ -19,6 +19,12 @@ if ( ! class_exists( 'WC_SiftScience_Html' ) ) :
 	 * Class WC_SiftScience_Html
 	 */
 	class WC_SiftScience_Html {
+		/**
+		 * The logger service
+		 *
+		 * @var WC_SiftScience_Logger
+		 */
+		private $logger;
 
 		public const WC_TITLE_ELEMENT      = 'title';
 		public const WC_TEXT_ELEMENT       = 'text';
@@ -27,6 +33,107 @@ if ( ! class_exists( 'WC_SiftScience_Html' ) ) :
 		public const WC_CHECKBOX_ELEMENT   = 'checkbox';
 		public const WC_SECTIONEND_ELEMENT = 'sectionend';
 		public const WC_CUSTOM_ELEMENT     = 'custom';
+
+		/**
+		 * WC_SiftScience_Html constructor.
+		 *
+		 * @param WC_SiftScience_Logger $logger Logger service.
+		 */
+		public function __construct( WC_SiftScience_Logger $logger ) {
+			$this->logger = $logger;
+		}
+
+		/**
+		 * This function sets HTML element attributes according to woocommearce provided library.
+		 * desc_tip Mixed [bool:false] (default)
+		 *     field type of checkbox; the desc text is going next to the control
+		 *     field type of select, number or text; the desc text is going underneath control
+		 * desc_tip Mixed [bool:true]
+		 *     [X] field type of checkbox; the desc text is going underneath control
+		 *     field type of select, number or text; a question mark pop-up appears before control with desc text
+		 * desc_tip Mixed [string]
+		 *     field type of checkbox; the desc_tip text is going underneath control
+		 *     field type of select, number or text; a question mark pop-up appears before control with desc_tip text
+		 * desc_tip is added in element options [X] sanitized.
+		 *
+		 * @param string $type            Element type name.
+		 * @param string $id              HtmlElement ID.
+		 * @param string $label           Element label.
+		 * @param string $desc            Description text.
+		 * @param array  $element_options Element special options.
+		 *
+		 * @return array $element_options An array of attributes.
+		 * @since 1.1.0
+		 */
+		public function create_element( $type, $id, $label = '', $desc = '', $element_options = array() ) {
+
+			// $element_options must have level 2 options array forb the select element.
+			if ( self::WC_SELECT_ELEMENT === $type ) {
+				if ( ! isset( $element_options['options'] ) || empty( $element_options['options'] ) ) {
+					$this->logger->log_error( 'Drop down ' . $id . ' cannot be empty!' );
+					return;
+				}
+			}
+
+			switch ( func_num_args() ) {
+				case 5:
+					if ( ! is_array( $element_options ) ) { // elsment_options must be an array.
+						$this->logger->log_error( 'element_options must be an array' );
+						return;
+					}
+					// array flattener.
+					$custom_attributes = array( 'min', 'max', 'step' );
+					$custom_attributes = array_intersect_key( $element_options, array_flip( $custom_attributes ) ); // Gets the new values.
+
+					if ( ! empty( $custom_attributes ) ) {
+						$element_options = array_diff_key( $element_options, $custom_attributes ); // Unsets those specific keys.
+
+						$element_options['custom_attributes'] = $custom_attributes; // sets array level 2.
+					}
+
+					if ( isset( $element_options['desc_tip'] ) ) {
+						if ( self::WC_CHECKBOX_ELEMENT === $type ) {
+							// if desc_tip is not a string or empty it is sanitized to false.
+							$desc_tip = $element_options['desc_tip']; // Temporary storege.
+
+							$element_options['desc_tip'] = ( is_string( $desc_tip ) && ! empty( $desc_tip ) ) ? $desc_tip : false;
+						}
+					}
+					// element_options exists so there is a description.
+				case 4:
+					$desc = ( empty( $desc ) ) ? '[Empty description]' : $desc;
+					// description exists so there is a label.
+				case 3:
+					$label = ( empty( $label ) ) ? '[Empty lable]' : $label;
+			}
+
+			switch ( $type ) {
+				case self::WC_CUSTOM_ELEMENT:
+					$type = 'wc_sift_' . $id; // this is the custom type name needed by WooCommerce.
+					add_action( 'woocommerce_admin_field_' . $type, array( $this, 'display_custom_settings_row' ) );
+					// This intentionally falls through to the next section.
+
+				case self::WC_TEXT_ELEMENT:
+				case self::WC_SELECT_ELEMENT:
+				case self::WC_NUMBER_ELEMENT:
+				case self::WC_CHECKBOX_ELEMENT:
+				case self::WC_TITLE_ELEMENT:
+					$element_options['desc']  = $desc;
+					$element_options['title'] = $label;
+					// Title and desc are added all What's left [id and type].
+
+				case self::WC_SECTIONEND_ELEMENT:
+					$element_options['id']   = $id;
+					$element_options['type'] = $type;
+					break;
+
+				default:
+					$this->logger->log_error( $type . ' is not a valid type!' );
+					break;
+			}
+
+			return $element_options;
+		}
 
 		/**
 		 * This function displayes sections in a bar separated list in regards of the current section
