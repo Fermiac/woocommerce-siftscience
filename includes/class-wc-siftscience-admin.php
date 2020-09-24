05399f83ce87c7d667643774ba1279656f06a9a6
ramico <ramico@gmail.com>
2020-09-24 05:57:53 +0300

    Use plugin_dir_url instead of plugin_url (#335)

--- includes/class-wc-siftscience-admin.php
+++ includes/class-wc-siftscience-admin.php
@@ -108,7 +108,7 @@ if ( ! class_exists( 'WC_SiftScience_Admin' ) ) :
 		 */
 		private static function enqueue_script( $name, $file, $deps ) {
 			$version = time(); // TODO: Make this switchable for dev purposes.
-			$path    = plugins_url( "dist/$file.js", dirname( __FILE__ ) );
+			$path    = plugin_dir_url( __DIR__ ) . "dist/$file.js";
 			wp_enqueue_script( $name, $path, $deps, $version, true );
 		}
 
