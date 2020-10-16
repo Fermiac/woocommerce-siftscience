a369299a12da8413bb92d1fde8ea873dbaeedd5b
ramico <ramico@gmail.com>
2020-10-16 18:36:17 +0300

    copy button text changes (#348)

--- dist/js/copy.js
+++ dist/js/copy.js
@@ -1,8 +1,19 @@
-function copyInfo( elementId ) {
-  var element = document.getElementById( elementId );
+window._wc_siftsci_js_sender = null;
+function copyInfo( source, elementId ) {
 
-  element.select();
-  document.execCommand( 'copy' );
-  
-  element.setSelectionRange( 0, 0 ); // Clear selection.
+	var sender = window._wc_siftsci_js_sender;
+
+	if( null !== sender ) {
+		sender.title = 'Copy to clipboard';
+	}
+	
+	window._wc_siftsci_js_sender = source;
+
+	var element = document.getElementById( elementId );
+
+	element.select();
+	document.execCommand( 'copy' );
+
+	element.setSelectionRange( 0, 0 ); // Clear selection.
+	source.title = 'Copied';
 }
